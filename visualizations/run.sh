46ffc05a3c4e6879545442ac405ac2915358e62f
Rishi Agarwal <rishiagarwal@172-16-26-100.DYNAPOOL.NYU.EDU>
2017-04-17 14:46:06 -0400

    Changes in graph script

--- visualizations/run.sh
+++ visualizations/run.sh
@@ -4,8 +4,13 @@ spark-submit Date.py /user/rda311/clean.csv
 spark-submit keycode.py /user/rda311/clean.csv
 spark-submit prem_desc.py /user/rda311/clean.csv
 spark-submit Time.py /user/rda311/clean.csv
-hadoop fs -getmerge borough_name.out
-hadoop fs -getmerge Date.out
-hadoop fs -getmerge keycode.out
-hadoop fs -getmerge prem_desc.out
-hadoop fs -getmerge Time.out
+hadoop fs -getmerge borough_name.out borough_name.out
+hadoop fs -getmerge Date.out Date.out
+hadoop fs -getmerge keycode.out keycode.out
+hadoop fs -getmerge prem_desc.out prem_desc.out
+hadoop fs -getmerge Time.out Time.out
+hadoop fs -rm -r borough_name.out
+hadoop fs -rm -r Date.out
+hadoop fs -rm -r keycode.out
+hadoop fs -rm -r prem_desc.out
+hadoop fs -rm -r Time.out

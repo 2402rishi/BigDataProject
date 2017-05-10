e6f02c31b2fb7e41da88a25fb46a18976f9c6c53
Rishi Agarwal <rishiagarwal@172-16-24-142.DYNAPOOL.NYU.EDU>
2017-05-09 20:14:12 -0400

    Changes in brooklyn date

--- visualizations/spark/brooklyn_date.py
+++ visualizations/spark/brooklyn_date.py
@@ -12,15 +12,10 @@ if __name__ == "__main__":
 	lines = sc.textFile(sys.argv[1], 1)
 	rows = lines.mapPartitions(lambda x: reader(x))
 	violation = rows.filter(lambda x : (str(x[13]) == "BROOKLYN"))
-	violation_code = violation.map(lambda x: ((x[1]),1))
-	violation_count = violation_code.reduceByKey(add)
-	
+	violation_code = violation.map(lambda x: ((x[1]),1)).reduceByKey(lambda x,y:x+y)
+	counts = violation_count.map(lambda x: str(x[0]).replace("'","").replace('(','').replace(')','') + '\t' + str(x[1]))
 
-    #convert Voilation_count to RDD and dictionary for formatting
-    #result = sc.parallelize(Voilation_count)
-    counts = violation_count.map(lambda x: str(x[0]).replace("'","").replace('(','').replace(')','') + '\t' + str(x[1]))
+	counts.saveAsTextFile("DateinBrooklyn.out")
 
-    counts.saveAsTextFile("DateinBrooklyn.out")
-
-    sc.stop()
+	sc.stop()
 
